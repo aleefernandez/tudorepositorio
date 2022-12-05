4b09203ec7a5855738b6517605678e5f8ab0513e
Alejandro Fernandez <alejandrofernandez45@gmail.com>
2022-12-05 09:37:50 -0300

    mas kambios

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// alert("hola, cómo ESTÁS?");
+// let numeroIngresado; 
+// do {
+//   numeroIngresado = prompt("Ingrese un numero:");
+//   if (numeroIngresado == "salir")
+//   {
+
+//   }
+//   else if (isNaN(numeroIngresado))
+//   {
+//     alert("Por favor ingrese un número");
+//   }
+//   else
+//   {
+
+//     let aux = 1;
+//     let listaDeNumeros = "";
+    
+//     while (aux <= numeroIngresado) {
+//       if (aux % 2 == 0) {
+//       } else {
+//         listaDeNumeros += aux + " ";
+//       }
+//       aux++;
+//     }
+    
+//     alert(listaDeNumeros);
+//   }
+// } while (numeroIngresado != "salir");
